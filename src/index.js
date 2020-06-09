5adb604fb976ef9d6ea59f9dd8211849c0226b46
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2020-06-09 12:11:35 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import AppComponent from './App/index.vue';
+let vm = new Vue({
+  el: '#app',
+  components: {
+    app: AppComponent,
+  },
+  render: (h) => h('app'),
+});
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
